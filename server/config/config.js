7e7b231504f2f2bde9fd733c7b06c50deb531a9f
Matias Bonino <matias2301@hotmail.com>
2018-11-11 20:44:24 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,24 @@
 //====================
 
 process.env.PORT = process.env.PORT || 3000;
+
+//====================
+// Entorno
+//====================
+
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//====================
+// Base de datos
+//====================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe_user:user1234@ds159273.mlab.com:59273/cafe';
+}
+
+process.env.URLDB = urlDB;
